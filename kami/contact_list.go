dc23c2984574ee4a2ce99300bf5294bb2d1a0b30
Maksimov Sergey <konjoot@gmail.com>
2015-10-26 17:34:44 +0300

    Update ContactList struct
    Change ContactList type to struct. Move []*Contact into the
    inner field list. Add new int field i for counting.
    
    Implement new methods:
    
    * Append - appends new *Contact to the inner list
    * Ids - returs SQL-like array literal for ids in the inner list
    * Next - increments inner counter i
    * Current - returns current element by counter i
    * Items - returns innter list

--- kami/contact_list.go
+++ kami/contact_list.go
@@ -1,7 +1,74 @@
 package main
 
+import (
+	"bytes"
+	"log"
+	"strconv"
+)
+
 func NewContactList(size int) ContactList {
-	return make(ContactList, 0, size)
+	return ContactList{i: -1, list: make([]*Contact, 0, size)}
 }
 
-type ContactList []*Contact
+type ContactList struct {
+	i    int
+	list []*Contact
+}
+
+func (c *ContactList) Next() bool {
+	c.i++
+
+	if len(c.list) < c.i+1 {
+		c.i--
+		return false
+	}
+
+	return true
+}
+
+func (c ContactList) Current() *Contact {
+	if c.i < 0 {
+		return nil
+	}
+
+	if len(c.list) < c.i+1 {
+		return nil
+	}
+
+	return c.list[c.i]
+}
+
+func (c *ContactList) Append(contact *Contact) {
+	c.list = append(c.list, contact)
+}
+
+func (c ContactList) Ids() string {
+	ids := bytes.NewBuffer([]byte(`{`))
+
+	strings := make([][]byte, 0)
+	for c.Next() {
+		if id, err := c.Current().Id.Value(); err == nil {
+			strings = append(strings, []byte(strconv.FormatInt(id.(int64), 10)))
+		}
+	}
+	result := bytes.Join(strings, []byte(`,`))
+
+	if _, err := ids.Write(result); err != nil {
+		log.Print(err)
+	}
+
+	if _, err := ids.Write([]byte(`}`)); err != nil {
+		log.Print(err)
+		return "{}"
+	}
+
+	return ids.String()
+}
+
+func (c ContactList) Any() bool {
+	return len(c.list) > 0
+}
+
+func (c ContactList) Items() []*Contact {
+	return c.list
+}
