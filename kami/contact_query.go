5e864ed19c9412cbae390ffca094eb6cab25aa08
Maksimov Sergey <konjoot@gmail.com>
2015-10-26 17:41:10 +0300

    Update ContactQuery struct
    Now it returns data for related profiles for each user

--- kami/contact_query.go
+++ kami/contact_query.go
@@ -24,24 +24,39 @@ func NewContactQuery(page int, perPage int) *ContactQuery {
 type ContactQuery struct {
 	limit      int
 	offset     int
-	collection []*Contact
+	collection ContactList
 	conn       *sql.DB
 }
 
-func (cq *ContactQuery) All() ContactList {
+func (cq *ContactQuery) All() []*Contact {
 	if cq.conn = NewDBConn(); cq.conn != nil {
 		defer cq.conn.Close()
 	}
 
-	err := cq.fillUsers()
-	if err != nil {
-		log.Print(err)
-		return NewContactList(0)
+	if ok := cq.fillUsers(); !ok {
+		return NewContactList(0).Items()
 	}
-	return cq.collection
+
+	if err := cq.fillDependentData(); err != nil {
+		log.Print(err)
+	}
+
+	return cq.collection.Items()
 }
 
-func (cq *ContactQuery) fillUsers() (err error) {
+func (cq *ContactQuery) fillUsers() (ok bool) {
+	var err error
+
+	defer func() {
+		if err != nil {
+			log.Print(err)
+		}
+
+		if cq.collection.Any() {
+			ok = true
+		}
+	}()
+
 	ps, err := cq.selectUsersStmt()
 	if err != nil {
 		return
@@ -66,7 +81,50 @@ func (cq *ContactQuery) fillUsers() (err error) {
 			&contact.Sex,
 		)
 
-		cq.collection = append(cq.collection, contact)
+		cq.collection.Append(contact)
+	}
+
+	return
+}
+
+func (cq *ContactQuery) fillDependentData() (err error) {
+	ps, err := cq.selectDependentDataStmt()
+	if err != nil {
+		return
+	}
+	defer ps.Close()
+
+	rows, err := ps.Query(cq.collection.Ids())
+	if err != nil {
+		return
+	}
+	defer rows.Close()
+
+	var userId sql.NullInt64
+	var current *Contact
+
+	if ok := cq.collection.Next(); ok {
+		current = cq.collection.Current()
+	}
+
+	for rows.Next() {
+
+		profile := NewProfile()
+		rows.Scan(
+			&profile.Id,
+			&profile.Type,
+			&userId,
+		)
+
+		for current.Id != userId {
+
+			if ok := cq.collection.Next(); !ok {
+				break
+			}
+			current = cq.collection.Current()
+		}
+
+		current.Profiles = append(current.Profiles, profile)
 	}
 
 	return
@@ -78,16 +136,31 @@ func (cq *ContactQuery) selectUsersStmt() (*sql.Stmt, error) {
 	}
 
 	return cq.conn.Prepare(`
-    select  id,
-            email,
-            first_name,
-            last_name,
-            middle_name,
-            date_of_birth,
-            sex
-      from users
-      where deleted_at is null
-      order by id
-      limit $1
-      offset $2`)
+		select	id,
+						email,
+						first_name,
+						last_name,
+						middle_name,
+						date_of_birth,
+						sex
+			from users
+			where deleted_at is null
+			order by id
+			limit $1
+			offset $2`)
+}
+
+func (cq *ContactQuery) selectDependentDataStmt() (*sql.Stmt, error) {
+	if cq.conn == nil {
+		return nil, errors.New("Can't connect to DB")
+	}
+
+	return cq.conn.Prepare(`
+		select	id,
+						type,
+						user_id
+			from profiles
+			where deleted_at is null
+				and user_id = any($1::integer[])
+			order by user_id, id`)
 }
