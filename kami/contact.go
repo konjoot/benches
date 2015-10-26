32d22ebf667796919dd05e8a7028ca1e0c592952
Maksimov Sergey <konjoot@gmail.com>
2015-10-26 17:30:59 +0300

    Update Contact struct
    * method MarshalJSON now calls shared MarshalJSON function
    * add Profiles field

--- kami/contact.go
+++ kami/contact.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"bytes"
 	"database/sql"
-	"database/sql/driver"
-	"encoding/json"
-	"reflect"
 
 	"github.com/lib/pq"
 )
@@ -22,49 +18,9 @@ type Contact struct {
 	MiddleName  sql.NullString
 	DateOfBirth pq.NullTime
 	Sex         sql.NullInt64
+	Profiles    []*Profile
 }
 
-func (c Contact) MarshalJSON() (res []byte, err error) {
-	defer func() {
-		if e := recover(); e != nil {
-			if ee, ok := e.(error); ok {
-				err = ee
-			}
-		}
-	}()
-
-	buffer := bytes.NewBuffer([]byte("{"))
-
-	rtype := reflect.TypeOf(&c).Elem()
-	rval := reflect.ValueOf(&c).Elem()
-	count := rtype.NumField()
-
-	result := make([][]byte, 0)
-
-	for i := range make([]struct{}, count) {
-		key := rtype.Field(i)
-		value := rval.Field(i).Interface()
-		if val, ok := value.(driver.Valuer); ok {
-			if v, err := val.Value(); v != nil && err == nil {
-				if vJson, err := json.Marshal(v); err == nil {
-					res := []byte(`"`)
-					res = append(res, []byte(key.Name)...)
-					res = append(res, []byte(`":`)...)
-					res = append(res, vJson...)
-					result = append(result, res)
-				}
-			}
-		}
-	}
-
-	jsonData := bytes.Join(result, []byte(","))
-	if _, err = buffer.Write(jsonData); err != nil {
-		return
-	}
-
-	if _, err = buffer.Write([]byte(`}`)); err != nil {
-		return
-	}
-
-	return buffer.Bytes(), err
+func (c *Contact) MarshalJSON() ([]byte, error) {
+	return MarshalJSON(c)
 }
