910198e83525845a0d2b5a95d519242b04ac47c4
Maksimov Sergey <konjoot@gmail.com>
2015-10-27 12:16:39 +0300

    [kami] Add ClassUnit struct

---
+++ kami/class_unit.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"database/sql"
+	"database/sql/driver"
+	"errors"
+
+	"github.com/lib/pq"
+)
+
+type ClassUnit struct {
+	Id         sql.NullInt64
+	Name       sql.NullString
+	EnlistedOn pq.NullTime
+	LeftOn     pq.NullTime
+}
+
+func (cu *ClassUnit) MarshalJSON() ([]byte, error) {
+	return MarshalJSON(cu)
+}
+
+func (cu ClassUnit) Value() (driver.Value, error) {
+	if cu.Id.Valid || cu.Name.Valid || cu.EnlistedOn.Valid || cu.LeftOn.Valid {
+		return &cu, nil
+	}
+
+	return nil, nil
+}
