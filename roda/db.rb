6979cc9e85afa614f6b35ea323a3ae168aeb9e86
Maksimov Sergey <konjoot@gmail.com>
2015-11-03 13:31:58 +0300

    [roda] Add db connection options

---
+++ roda/db.rb
@@ -0,0 +1,9 @@
+module Main
+    DB_PARAMS = {
+      adapter:   'postgres',
+      host:      'localhost',
+      database:  'lms2_development_2',
+      user:      'lms',
+      password:  ''
+    }
+end
