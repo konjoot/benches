d8efa23a67b426ab4a5994ca96e590fab883394a
Maksimov Sergey <konjoot@gmail.com>
2015-11-03 13:34:02 +0300

    [roda] Add Contact class

---
+++ roda/contact.rb
@@ -0,0 +1,19 @@
+require './jsonable'
+
+module Main
+  class Contact < Jsonable
+    attr_reader :id,
+                :email,
+                :first_name,
+                :last_name,
+                :middle_name,
+                :date_of_birth,
+                :sex
+
+    def initialize(attrs)
+      attrs.each do |key, val|
+        instance_variable_set("@#{key}", val)
+      end
+    end
+  end
+end
