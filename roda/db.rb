b06d06f0e22653d3f41a18bff8d97f44424f99af
konjoot <konjoot@gmail.com>
2015-11-04 12:04:44 +0300

    [roda] Create DB module
    It holds db pool

--- roda/db.rb
+++ roda/db.rb
@@ -1,9 +1,19 @@
-module Main
-    DB_PARAMS = {
-      adapter:   'postgres',
-      host:      'localhost',
-      database:  'lms2_development_2',
-      user:      'lms',
-      password:  ''
-    }
+module DB
+  DB_PARAMS = {
+    adapter:   'postgres',
+    host:      'localhost',
+    database:  'lms2_db_dev',
+    user:      'lms2_db_user',
+    password:  'lms_2014'
+  }
+
+  @@conn = nil
+
+  private
+
+  def db
+    @@conn ||= Sequel.connect(DB_PARAMS)
+  end
+
+  module_function :db
 end
