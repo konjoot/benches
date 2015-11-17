8bf0f9693c329fc2fdd5aea315024c5b89aaa1cc
Maksimov Sergey <konjoot@gmail.com>
2015-11-17 16:17:31 +0300

    [roda][School] Speed up encoding to json

--- roda/school.rb
+++ roda/school.rb
@@ -1,11 +1,21 @@
+require "oj"
+
 module Main
-  class School < Jsonable
-    attr_reader :id, :guid, :name
+  class School
 
     def initialize(id:, guid:, name:)
-      @id = id
-      @guid = guid
-      @name = name
+      @hash = {}
+      @hash[:Id] = id if id
+      @hash[:Guid] = guid if guid
+      @hash[:Name] = name if name
+    end
+
+    def to_hash
+      @hash
+    end
+
+    def to_json
+      Oj.dump(@hash)
     end
   end
 end
