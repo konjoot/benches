2aa49f1d55e40347c6caa77aa7503a5e7d8dbaa6
Maksimov Sergey <konjoot@gmail.com>
2015-11-17 16:22:34 +0300

    [roda][App] Speed up encoding to json

--- roda/app.rb
+++ roda/app.rb
@@ -1,19 +1,21 @@
 require "roda"
+require "oj"
 
 require "./contact_query"
 
 module Main
   class App < Roda
     use Rack::Session::Cookie, secret: "v3rYstr0n9S3cr3t;)"
+    Oj.default_options = {:mode => :compat }
 
     plugin :indifferent_params
     plugin :default_headers,
-      'Content-Type' => 'application/json; charset=utf-8'
+      "Content-Type" => "application/json; charset=utf-8"
 
     route do |r|
       # /contacts
       r.get "contacts" do
-        ContactQuery.new(params).all().to_json
+        Oj.dump ContactQuery.new(params).all()
       end
     end
   end
