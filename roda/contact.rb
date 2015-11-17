1aa31aaeae057c099483a2a44fab48fe9284271c
Maksimov Sergey <konjoot@gmail.com>
2015-11-17 16:18:40 +0300

    [roda][Contact] Speed up encoding to json

--- roda/contact.rb
+++ roda/contact.rb
@@ -1,15 +1,8 @@
-require "./jsonable"
+require "oj"
 
 module Main
-  class Contact < Jsonable
-    attr_reader :id,
-                :email,
-                :first_name,
-                :last_name,
-                :middle_name,
-                :date_of_birth,
-                :sex,
-                :profiles
+  class Contact
+    attr_reader :profiles
 
     def initialize(
       id:,
@@ -20,14 +13,28 @@ module Main
       date_of_birth:,
       sex:
     )
-      @id = id
-      @email = email
-      @first_name = first_name
-      @last_name = last_name
-      @middle_name = middle_name
-      @date_of_birth = date_of_birth
-      @sex = sex
+      @hash = {}
+      @hash[:Id] = id if id
+      @hash[:Email] = email if email
+      @hash[:FirstName] = first_name if first_name
+      @hash[:LastName] = last_name if last_name
+      @hash[:MiddleName] = middle_name if middle_name
+      @hash[:DateOfBirth] = date_of_birth if date_of_birth
+      @hash[:Sex] = sex if sex
       @profiles = []
+    end
+
+    def id
+      @hash[:Id]
+    end
+
+    def to_hash
+      @hash[:Profiles] = @profiles if @profiles[0]
+      @hash
+    end
+
+    def to_json
+      Oj.dump(to_hash)
     end
   end
 end
