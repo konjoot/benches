d9e7c69a5c65a7c4332ad1918e965f794fae2392
Maksimov Sergey <konjoot@gmail.com>
2015-11-17 16:21:22 +0300

    [roda][DB] Unify quotes

--- roda/db.rb
+++ roda/db.rb
@@ -1,10 +1,10 @@
 module DB
   DB_PARAMS = {
-    adapter:   'postgres',
-    host:      'localhost',
-    database:  'lms2_development_2',
-    user:      'lms',
-    password:  ''
+    adapter:   "postgres",
+    host:      "localhost",
+    database:  "lms2_development_2",
+    user:      "lms",
+    password:  ""
   }
 
   @@conn = nil
