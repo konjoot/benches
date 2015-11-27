fa15a45fe94c2e25e3a7d73a1edd1cf881dc1431
Maksimov Sergey <konjoot@gmail.com>
2015-11-27 15:25:29 +0300

    [kami][ClassUnit] Use pointer fields

--- kami/class_unit.go
+++ kami/class_unit.go
@@ -1,27 +1,14 @@
 package main
 
-import (
-	"database/sql"
-	"database/sql/driver"
+import "time"
 
-	"github.com/lib/pq"
-)
+func NewClassUnit() *ClassUnit {
+	return &ClassUnit{}
+}
 
 type ClassUnit struct {
-	Id         sql.NullInt64
-	Name       sql.NullString
-	EnlistedOn pq.NullTime
-	LeftOn     pq.NullTime
-}
-
-func (cu *ClassUnit) MarshalJSON() ([]byte, error) {
-	return MarshalJSON(cu)
-}
-
-func (cu ClassUnit) Value() (driver.Value, error) {
-	if cu.Id.Valid || cu.Name.Valid || cu.EnlistedOn.Valid || cu.LeftOn.Valid {
-		return &cu, nil
-	}
-
-	return nil, nil
+	Id         *int32     `json:"id,omitempty"`
+	Name       *string    `json:"name,omitempty"`
+	EnlistedOn *time.Time `json:"enlistedOn,omitempty"`
+	LeftOn     *time.Time `json:"leftOn,omitempty"`
 }
