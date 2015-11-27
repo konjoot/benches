e08fad131d68ce14c94e02113667aa2aa0bcab49
Maksimov Sergey <konjoot@gmail.com>
2015-11-27 15:27:24 +0300

    [kami][ContactList] Refactor

--- kami/contact_list.go
+++ kami/contact_list.go
@@ -1,62 +1,45 @@
 package main
 
-import (
-	"bytes"
-	"log"
-	"strconv"
-)
-
 func NewContactList(size int) ContactList {
-	return ContactList{i: -1, list: make([]*Contact, 0, size)}
+	return ContactList{i: -1, Items: make([]*Contact, 0, size)}
 }
 
 type ContactList struct {
-	i    int
-	list []*Contact
+	i     int
+	Items []*Contact
 }
 
 func (c *ContactList) Next() *Contact {
 	c.i++
 
-	if len(c.list) < c.i+1 {
+	if len(c.Items) < c.i+1 {
 		c.i--
 		return nil
 	}
 
-	return c.list[c.i]
-}
-
-func (c *ContactList) Append(contact *Contact) {
-	c.list = append(c.list, contact)
-}
-
-func (c ContactList) Ids() []byte {
-	ids := bytes.NewBuffer([]byte("{"))
-
-	strings := make([][]byte, 0)
-	for _, next := range c.list {
-		if id, err := next.Id.Value(); err == nil {
-			strings = append(strings, []byte(strconv.FormatInt(id.(int64), 10)))
-		}
-	}
-	result := bytes.Join(strings, []byte(","))
-
-	if _, err := ids.Write(result); err != nil {
-		log.Print(err)
-	}
-
-	if _, err := ids.Write([]byte("}")); err != nil {
-		log.Print(err)
-		return []byte("{}")
-	}
-
-	return ids.Bytes()
+	return c.Items[c.i]
 }
 
 func (c ContactList) Any() bool {
-	return len(c.list) > 0
+	return len(c.Items) > 0
 }
 
-func (c ContactList) Items() []*Contact {
-	return c.list
+func (c ContactList) Ids() []int32 {
+	arr := make([]int32, len(c.Items))
+
+	for i, contact := range c.Items {
+		arr[i] = *contact.Id
+	}
+
+	return arr
+}
+
+func (c ContactList) IntIds() []int {
+	arr := make([]int, len(c.Items))
+
+	for i, contact := range c.Items {
+		arr[i] = int(*contact.Id)
+	}
+
+	return arr
 }
